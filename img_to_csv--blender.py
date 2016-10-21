bc7122c2c19113ee613b4933fe29fb1b12186a11
Léo Martin <bonjour@leomartin.net>
2016-10-21 19:10:42 +0200

    v. 1 blender

--- img_to_csv--blender.py
+++ img_to_csv--blender.py
@@ -4,7 +4,12 @@
 import sys, os, bpy, math
 from os.path import basename
 
-path = '/Users/leo/Travail/ongoing/A.009. Script illustrator François/test-img.jpg'
+path = 'chemin/vers/le/fichier'
+
+def print_status(current, maximum):
+    output = "\ravancement : " + str( int( current / maximum * 100 ) ) + "%"
+    sys.stdout.write(output)
+    sys.stdout.flush()
 
 def pixel_values(image, x, y):
     '''Return the RGB values of a given pixel in a given image,
@@ -15,7 +20,7 @@ def pixel_values(image, x, y):
     w = image.size[0] # image height
     h = image.size[1] # image width
     c = 4; # colour canals : RGBA = 4
-    base = ( w * c ) * ( h - y - 1) + ( x * c ) # Pixel information start at this position
+    base = ( w * c ) * ( h - x - 1) + ( y * c ) # Pixel information start at this position
     R_pos = base + 0 ; # R
     G_pos = base + 1; # G
     B_pos = base + 2; # B
@@ -23,21 +28,24 @@ def pixel_values(image, x, y):
     return [ int( pixels[R_pos] * 255), int( pixels[G_pos] * 255), int(  pixels[B_pos] * 255) ]
 
 def main():
-
     bpy.ops.image.open(filepath=path)
     for image in bpy.data.images:
         directory = os.path.dirname(image.filepath)
         filename = os.path.splitext(image.filepath)[0].split("/")[-1]
-        pixels = "";
-        for y in range(image.size[1]):
-            for x in range(image.size[0]):
-                pixels += str( pixel_values(image, x, y) )
-                if x < image.size[1]:
-                    pixels += "|"
-            if y < image.size[0]-1:
-                pixels += "\n"
+        print("traitement de" + filename)
         file = open(directory+"/"+filename+".csv", "w")
-        file.write(pixels)
+        file.seek(0)
+        file.truncate()
+        for y in range(image.size[0]):
+            csv_line = "";
+            for x in range(image.size[1]):
+                csv_line += str( pixel_values(image, x, y) )
+                if x < image.size[1] - 1:
+                    csv_line += "|"
+            file.write(csv_line + "\n")
+            print_status(y, image.size[1])
         file.close()
+        print_status(1,1)
+        print("\n")
 
 main()
